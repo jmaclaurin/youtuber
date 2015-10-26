6a23db4131cc9a8eb73adf317333c54680941d5a
Jessica Maclaurin <jessica.maclaurin@epiuse.com>
2015-10-26 17:38:46 +0200

    Frontend actions for creating and editing youtube links

--- web/js/script.js
+++ web/js/script.js
@@ -1,12 +1,29 @@
 var youtuber = angular.module('youtuber', ['ngRoute']);
-
 youtuber.config(function($routeProvider){
 	$routeProvider.when('/new', {
 		controller: 'YoutubeLinksController',
-		templateUrl: 'new.html'
+		templateUrl: 'form.html',
+		resolve: {
+			data: function(){
+				return null;
+			}
+		}
+	}).when('/edit/:youtube_link_id', {
+		controller: 'YoutubeLinksController',
+		templateUrl: 'form.html',
+		resolve: {
+			data: function(youtubeLinksFactory, $route){
+        		return youtubeLinksFactory.getYoutubeLink($route.current.params.youtube_link_id);
+			}
+		}
 	}).when('/', {
 		controller: 'YoutubeLinksController',
-		templateUrl: 'list.html'
+		templateUrl: 'list.html',
+		resolve: {
+			data: function(youtubeLinksFactory, $route){
+        		return null;
+			}
+		}
 	}).otherwise({redirectTo: '/'});
 });
 
@@ -23,46 +40,52 @@ youtuber.factory('youtubeLinksFactory', ['$http', function($http){
 	};
 
 	factory.saveYoutubeLink = function(youtubeLink){
-		return $http.post(urlBase + 'youtube_links', youtubeLink);
+		if(youtubeLink.id){
+			return $http.put(urlBase + 'youtube_links/' + youtubeLink.id, youtubeLink)
+		}else{
+			return $http.post(urlBase + 'youtube_links', youtubeLink);
+		}
 	};
 
 	factory.deleteYoutubeLink = function(id){
 		return $http.delete(urlBase + 'youtube_links/' + id);
 	};
 
-	factory.updateYoutubeLink = function(youtubeLink){
-		return $http.put(urlBase + 'youtube_links/' + youtubeLink.id, youtubeLink)
-	};
-
 	return factory;
 }]);
 
-youtuber.controller('YoutubeLinksController', ['$scope', 'youtubeLinksFactory', function($scope, youtubeLinksFactory){
+youtuber.controller('YoutubeLinksController', ['$scope', '$routeParams', '$location', 'youtubeLinksFactory', 'data', function($scope, $routeParams, $location, youtubeLinksFactory, data){
 	$scope.youtube_links;
 	$scope.status;
 
+	if(data){
+		$scope.youtubeLink = data.data;
+	}
+
 	getYoutubeLinks();
+
 
     function getYoutubeLinks() {
         youtubeLinksFactory.getYoutubeLinks()
             .success(function(links){
                 $scope.youtube_links = links;
-                console.log($scope.youtube_links);
             })
             .error(function(error){
                 $scope.status = 'Unable to load youtube links data: ' + error.message;
             });
     };
 
-    $scope.getYoutubeLink = function(id){
-
-    };
-
     $scope.saveYoutubeLink = function(){
     	youtubeLinksFactory.saveYoutubeLink($scope.youtubeLink).success(function(){
-    		// Do some stuff
+    		$scope.status = 'Successfully saved youtube link';
+    		$location.path('/');
     	}).error(function(error){
+    		$scope.status = 'Unable to save youtube link: ' + error.message; 
     		// Do some stuff
     	});
+    };
+
+    $scope.removeYoutubeLink = function(){
+
     };
 }]);
